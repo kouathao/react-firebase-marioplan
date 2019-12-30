f92fd86fae29a9a5bcaa6cfd62eec57570e6fe59
Koua Thao <kouathao09@gmail.com>
2019-12-29 18:53:36 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Mario Plan</h1>
-      </div>
+      <Router>
+        <div>
+          <h1>Mario Plan</h1>
+        </div>
+      </Router>
     );
   }
 }
